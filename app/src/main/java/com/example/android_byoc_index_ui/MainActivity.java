5cdd5153be58ee712664fc0aaabb49d082908afe
蔡昀辰 Cheney.Tsai <Cheney.Tsai@jet-opto.com.tw>
2020-02-24 17:43:33 +0800

    finish usb attention dialog

--- app/src/main/java/com/example/android_byoc_index_ui/MainActivity.java
+++ app/src/main/java/com/example/android_byoc_index_ui/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.android_byoc_index_ui;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -27,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         brightness = findViewById(R.id.brightness);
         volume = findViewById(R.id.volume);
         chlid_lock = findViewById(R.id.child_lock);
+
 
 
         mirror.setOnTouchListener(new View.OnTouchListener() {
@@ -100,6 +105,37 @@ public class MainActivity extends AppCompatActivity {
                         usb_files.setBackgroundResource(R.drawable.usb_files);
                     }
 
+                    // Build an AlertDialog
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    LayoutInflater inflater = getLayoutInflater();
+                    View dialogView = inflater.inflate(R.layout.alertdialog_custom_view,null);
+
+
+                    // Set the custom layout as alert dialog view
+                    builder.setView(dialogView);
+                    // Get the custom alert dialog view widgets reference
+                    Button btn_positive = (Button) dialogView.findViewById(R.id.dialog_ok);
+                    Button btn_negative = (Button) dialogView.findViewById(R.id.dialog_small_close);
+
+                    // Create the alert dialog
+                    final AlertDialog dialog = builder.create();
+                    // Set positive/yes button click listener
+                    btn_positive.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            // Dismiss the alert dialog
+                            dialog.cancel();
+                        }
+                    });
+                    btn_negative.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            // Dismiss the alert dialog
+                            dialog.cancel();
+                        }
+                    });
+                    // Display the custom alert dialog on interface
+                    dialog.show();
                 }
                 return false;
             }
